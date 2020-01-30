f65973819a0b5a742026616b5a72024fd6ad1984
blee22 <blee22@hawkmail.hfcc.edu>
2020-01-30 18:18:22 -0500

    CIS172

---
+++ HW1/index.js
@@ -0,0 +1,63 @@
+
+let name = "Boram Lee";
+let major = "CIS";
+let hobby = "snowboarding";
+
+console.log( name );
+
+console.log( major );
+
+console.log( hobby);
+
+
+const email = "blee22@hawkmail.hfcc.edu";
+
+function preferred_email()
+{
+    var email = "blee22@hawkmail.hfcc.edu";
+    console.log(email);
+
+}
+
+preferred_email();
+
+
+const myclass= "CIS-172";
+
+
+console.log(myclass);
+
+let mySentance =
+    "I am using JS in " +myclass+".";
+console.log(mySentance);
+
+//for loop
+for (var i =0; i <11; i++)
+{
+    console.log("forloop:",i);
+}
+
+//while loop
+var j = 10;
+while (j <21)
+{
+    j++;
+    console.log("whileloop:",j);
+}
+
+//if else
+
+let myname = "boram";
+consol.log("Welcome" + myname + major + ".");
+
+if (myname === "Mr.Banks") ;
+{
+
+    { console.log("Welcome back Professor!");}
+}
+else
+{
+    console.log("Welcome" + myname + major + ".");
+}
+
+
---
+++ Quiz/quiz1.js
@@ -0,0 +1,36 @@
+// 1. Define a constant named favBook that holds the name of your favorite book.
+//      Also define a standard variables called favInt that contains your favorite int.
+
+
+let favBook = "Pride and Prejudice";
+let favInt = "27";
+
+
+console.log( favBook );
+
+console.log( favInt );
+
+//2. Write an if statement that meets the following conditions.
+//      If favInt is greater than 42, console log the word BIGGER.
+//      If favInt is less than 42, console log the word SMALLER.
+//      If favInt is equal to 42, console log the word EQUAL.
+
+
+
+
+if( favInt >= 42 )
+
+    {
+        console.log("BIGGER");
+    }
+
+}
+else if( favInt = 42 )
+{
+    console.log("EQUAL");
+}
+
+else
+{
+    console.log("SMALLER");
+}
