a247e045610b4ae4d9f17f383c4909b1befe74e8
blee22 <blee22@hawkmail.hfcc.edu>
2020-01-30 18:54:05 -0500

    CIS172

--- Quiz/quiz1.js
+++ Quiz/quiz1.js
@@ -15,22 +15,63 @@ console.log( favInt );
 //      If favInt is less than 42, console log the word SMALLER.
 //      If favInt is equal to 42, console log the word EQUAL.
 
+int =favInt;
 
 
-
-if( favInt >= 42 )
-
-    {
-        console.log("BIGGER");
-    }
-
+if (favInt >= 42) {
+    console.log("BIGGER");
 }
-else if( favInt = 42 )
-{
-    console.log("EQUAL");
-}
-
-else
-{
+else if (favInt < 42) {
     console.log("SMALLER");
 }
+else {
+    console.log("Equal");
+}
+
+
+// 3. Write a for loop that iterates 7 times. Log the name of your book in each iteration.
+
+//for loop
+
+for (var i =0; i <8; i++)
+{
+    console.log(favBook +":" +i);
+}
+
+
+
+// 4. Write a while loop that iterates until favInt is less than or equal to -42.
+//      Inside the loop, write an if that will send a console error if favInt is 0.
+//      The console error should read "ZERO DETECTED!"
+
+var j = 27;
+while (j <= 42)
+{
+    j++;
+    console.log("whileloop:",j);
+    else(j=0)
+}
+
+
+
+
+
+// 2 ex pts
+// 5. Write  for you favBook that meets the following bookname conditions.
+//      "The Matrix" should console log "Excellent Movie!"
+//      "Tears Of Steel" should log "I love open source!"
+//      "Fight Club" should log "Excellent Movie!"
+//      "Citizen Kane" should log "All Time Best Movie!"
+//      "Star Wars" should log "He didn't say Luke I am your father!"
+
+ const expr = favBook;
+ switch (expr) {
+    case "The Matrix":
+        console.log(favBook +"should console log Excellent Movie!");
+        break;
+    case "Tears of Steel":
+        console.log(favBook +"should console log I love open source!"');
+        break;
+
+
+ }
