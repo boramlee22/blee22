9ae612dfb1cfbba8c1e9950958234e81f3185b19
boramlee22 <60243695+boramlee22@users.noreply.github.com>
2020-02-11 23:59:11 -0500

    HW2

---
+++ HW2/calculator.js
@@ -0,0 +1,18 @@
+function _(x) {
+    return document.getElementById(x);
+}
+
+
+function calc(val){
+    _('display').value += val;
+}
+
+
+function c(val){
+    _('display').value = " ";
+}
+
+
+function e(val){
+    _('display').value = eval(_('display').value);
+}
