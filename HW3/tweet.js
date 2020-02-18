54cc0ea0b2791220f3cfa1a473d0e92c94f94957
blee22 <blee22@hawkmail.hfcc.edu>
2020-02-18 18:06:09 -0500

    HW3 1st

---
+++ HW3/tweet.js
@@ -0,0 +1,21 @@
+
+
+let tweetCollection = {};
+
+
+tweetCollection.tweetBuilder = function(){
+
+    let tweetUsing {};
+
+    tweetUsing.id       = ;
+    tweetUsing.message  = ;
+    tweetUsing.likes    = ;
+    tweetUsing.shares   = ;
+    tweetUsing.comments = ;
+    tweetUsing.date     = ;
+    tweetUsing.username = ;
+
+
+
+    _('display').value += val;
+}
