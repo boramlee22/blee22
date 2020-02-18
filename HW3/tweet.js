fa8d26e40ce814b6e86cb391a1d91762ae50582d
blee22 <blee22@hawkmail.hfcc.edu>
2020-02-18 18:43:44 -0500

    hw3

--- HW3/tweet.js
+++ HW3/tweet.js
@@ -2,20 +2,20 @@
 
 let tweetCollection = {};
 
+tweetCollection.firstpage = [];
 
 tweetCollection.tweetBuilder = function(){
 
     let tweetUsing {};
 
-    tweetUsing.id       = ;
-    tweetUsing.message  = ;
-    tweetUsing.likes    = ;
-    tweetUsing.shares   = ;
-    tweetUsing.comments = ;
-    tweetUsing.date     = ;
-    tweetUsing.username = ;
+    tweetUsing.id       = document.getElementById('id').value;;
+    tweetUsing.message  = document.getElementById('message').value;;
+    tweetUsing.likes    = document.getElementById('likes').value;;
+    tweetUsing.shares   = document.getElementById('shares').value;;
+    tweetUsing.comments = document.getElementById('comments').value;;
+    tweetUsing.username = document.getElementById('username').value;;
 
+    tweetCollection.firstpage.push(tweetUsing);
+    console.log(tweetUsing);
 
-
-    _('display').value += val;
 }
