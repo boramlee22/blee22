34386217f5639a044b6936d4c85f2af5e1ef44ee
boramlee22 <60243695+boramlee22@users.noreply.github.com>
2020-02-20 15:09:20 -0500

    HW3

--- HW3/tweet.js
+++ HW3/tweet.js
@@ -1,21 +1,35 @@
 
 
-let tweetCollection = {};
 
-tweetCollection.firstpage = [];
+
+let tweetCollection = { };
+
+
+tweetCollection.firstpage = ['id', 'message', 'likes', 'shares', 'comments', 'username'];
 
 tweetCollection.tweetBuilder = function(){
 
-    let tweetUsing {};
+    let tweetUsing = { };
 
-    tweetUsing.id       = document.getElementById('id').value;;
-    tweetUsing.message  = document.getElementById('message').value;;
-    tweetUsing.likes    = document.getElementById('likes').value;;
-    tweetUsing.shares   = document.getElementById('shares').value;;
-    tweetUsing.comments = document.getElementById('comments').value;;
-    tweetUsing.username = document.getElementById('username').value;;
+    tweetUsing.id       = document.getElementById('id').value;
+    tweetUsing.message  = document.getElementById('message').value;
+    tweetUsing.likes    = document.getElementById('likes').value;
+    tweetUsing.shares   = document.getElementById('shares').value;
+    tweetUsing.comments = document.getElementById('comments').value;
+    tweetUsing.username = document.getElementById('username').value;
 
     tweetCollection.firstpage.push(tweetUsing);
     console.log(tweetUsing);
 
 }
+
+let tweetBuilderBtn = document.getElementById("tweet");
+tweetBuilderBtn.addEventListener( "click", tweetCollection.tweetBuilder );
+
+
+var tweetCollection.firstpage = require
+
+
+
+
+console.log(tweetCollection.firstpage);
