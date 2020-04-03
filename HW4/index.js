02c475c87e9b2bf01ff78f1de7545a7b46ae8a10
boramlee22 <60243695+boramlee22@users.noreply.github.com>
2020-04-03 00:00:49 -0400

    HW4

--- HW4/index.js
+++ HW4/index.js
@@ -1,22 +1,44 @@
 var PageManager = {
 
-    myBtn = null,
 
     init : function(){
-        PageManager.myBtn = document.getElementById('btn1');
-        PageManager.myBtn.addEventListener("click", PageManeger.inputText );
-        PageManager.myBtn = document.getElementById('btn2');
-        PageManager.myBtn.addEventListener("click", PageManeger.inputText );
-        PageManager.myBtn = document.getElementById('btn3');
-        PageManager.myBtn.addEventListener("click", PageManeger.inputText );
+        let myBtn1 = document.getElementById('btn1');
+        myBtn1.addEventListener("click", PageManager.changeText );
+
+        PageManager.myBtn2 = document.getElementById('btn2');
+        PageManager.myBtn2.addEventListener("click", PageManager.inputDiv );
+        PageManager.myBtn3 = document.getElementById('btn3');
+        PageManager.myBtn3.addEventListener("click", PageManager.consoleObject );
     },
 
-
-    inputText: function (value)
+    changeText : function (event)
     {
-        document.getElementById('text1').innerHTML = text;
+        let textBox = document.getElementById('text1');
+        let newText = textBox.value;
+        let h1 = document.getElementById('h1');
+        h1.innerHTML = newText;
+    },
+    inputDiv : function()
+
+    {
+        var div = document.getElementById(appdiv);
+        var text = document.getElementById('text2');
+        div.appendChild('text');
+       document.body.appendChild('div');
+
+        //document.getElementById('appDiv').innerHTML += document.getElementById('text2');
     },
 
+    consoleObject: function ()
+    {
+        console.log(PageManager);
+    },
 
+    var select = document.getElementById('dropdown');
+    select.addEventListener('change',function(){
+        if (select.value ==='white'){
+            select.style.backgroundColor = "white";
+        }
+    }
 };
 window.onload = PageManager.init;
